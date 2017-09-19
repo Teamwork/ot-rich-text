5c967765bf7e6d61b927b39688d3f2bb3be55484
Greg Kubisa <gkubisa@gmail.com>
2017-09-19 12:15:00 +0100

    Implement diffX (WIP)

--- test/Delta.js
+++ test/Delta.js
@@ -355,3 +355,39 @@ tap.test('transformCursor', t => {
 
     t.end()
 })
+
+tap.test('diffX', t => {
+    t.throws(() => Delta.diffX([ createRetain(1) ], []), Error)
+    t.throws(() => Delta.diffX([ createDelete(1) ], []), Error)
+    t.throws(() => Delta.diffX([], [ createRetain(1) ]), Error)
+    t.throws(() => Delta.diffX([], [ createDelete(1) ]), Error)
+
+    const testDiff = (delta1, delta2) => {
+        const diff = [ result1, result2 ] = Delta.diffX(delta1, delta2)
+        t.strictSame(Delta.compose(delta1, result2), delta2)
+        t.strictSame(Delta.compose(delta2, result1), delta1)
+    }
+
+    testDiff([], [])
+    testDiff([
+        createInsertText('abc', 0, ''),
+        createInsertOpen('\uE000', 0, '', 'DIV'),
+        createInsertEmbed('\uE001', 0, '', 'IMG', [ 'src', 'http://www.example.com/image.png' ]),
+        createInsertClose('\uE002', 0, '', 'DIV')
+    ], [])
+    testDiff([], [
+        createInsertText('abc', 0, ''),
+        createInsertOpen('\uE000', 0, '', 'DIV'),
+        createInsertEmbed('\uE001', 0, '', 'IMG', [ 'src', 'http://www.example.com/image.png' ]),
+        createInsertClose('\uE002', 0, '', 'DIV')
+    ])
+    testDiff([
+        createInsertText('abc', 0, '')
+    ], [
+        createInsertText('a', 0, ''),
+        createInsertEmbed('\uE001', 0, '', 'IMG', [ 'src', 'http://www.example.com/image.png' ]),
+        createInsertText('c', 0, '')
+    ])
+
+    t.end()
+})
