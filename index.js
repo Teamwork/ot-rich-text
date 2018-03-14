a1091d917fb2d897747e6cdf25deaff3254de2da
Greg Kubisa <gkubisa@gmail.com>
2018-03-14 17:36:12 +0000

    Update names in index.js and type.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     type: require('./lib/type'),
     Iterator: require('./lib/Iterator'),
-    Operation: require('./lib/Action'),
-    Delta: require('./lib/Operation')
+    Action: require('./lib/Action'),
+    Operation: require('./lib/Operation')
 }
